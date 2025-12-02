e009159c44c7aafa5dbd0ced395b83d881682b44
Nicolas Despres <ndespres@gmail.com>
2025-12-02 06:08:31 +0100

    deploy to github pages

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/sf-doc-to-json/' : '/',
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
 
